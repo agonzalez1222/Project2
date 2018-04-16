32b202d0e03984aa138d7f0c26832c874a5cb745
Armando Gonzalez <agonzalez9@ggc.edu>
2018-04-15 21:37:49 -0400

    Project 2

---
+++ VehicleDBApi/src/main/java/com/example/vehicleapi/VehicleapiApplication.java
@@ -0,0 +1,14 @@
+package com.example.vehicleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class VehicleapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleapiApplication.class, args);
+    }
+}
